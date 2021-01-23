b12f0b31c8f8f0e1021252f21c1d8f56552c1a96
Duygu6 <pahliduygu@gmail.com>
2021-01-23 05:21:23 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+     //burada } olduğu için 15 tane hata verdi ve o küçücük hatayı bulmak için 1 saat kadar uğraştım. demek ki engin hocanın demek istediği buymuş:D,bunu okuyorsanız eğer başarılar dilerim:)
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item; 
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Duygu");
+            isimler.Add("Fatih");
+            
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+            List<int> sayilar = new List<int>();
+            sayilar.Add(2);
+            sayilar.Add(4);
+            Console.WriteLine(sayilar.Count);
+
+        }
+       }
+    }
+
